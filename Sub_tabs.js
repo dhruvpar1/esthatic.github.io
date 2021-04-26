a38891d5f10e04c3aad9f470e21e6dd27b12cec7
dhruvpar1 <83108871+dhruvpar1@users.noreply.github.com>
2021-04-26 16:58:41 +0530

    aesthatic
    MY FIRST WEBSITE

---
+++ Sub_tabs.js
@@ -0,0 +1,120 @@
+function f1() {
+    var video = document.getElementById('video')
+    var image = document.getElementById('image')
+
+
+    video.style.display = 'block'
+    image.style.display = 'none'
+    video.play()
+    setTimeout(function () {
+      video.pause()
+      video.currentTime = 0
+      video.style.display = 'none'
+      image.style.display = 'block'
+    }, 5000)
+  }
+  function f2() {
+    var vd = document.getElementById('vd')
+    var ig = document.getElementById('ig')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
+  function f3() {
+    var vd = document.getElementById('vd1')
+    var ig = document.getElementById('ig1')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
+  function f4() {
+    var vd = document.getElementById('vd4')
+    var ig = document.getElementById('ig4')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
+  function f5() {
+    var vd = document.getElementById('vd5')
+    var ig = document.getElementById('ig5')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
+  function f6() {
+    var vd = document.getElementById('vd6')
+    var ig = document.getElementById('ig6')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
+  function f7() {
+    var vd = document.getElementById('vd7')
+    var ig = document.getElementById('ig7')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
+  function f8() {
+    var vd = document.getElementById('vd8')
+    var ig = document.getElementById('ig8')
+
+
+    vd.style.display = 'block'
+    ig.style.display = 'none'
+    vd.play();
+    setTimeout(function () {
+      vd.pause()
+      vd.currentTime = 0
+      vd.style.display = 'none'
+      ig.style.display = 'block'
+    }, 5000)
+  }
